161d1b82479469de2bcccb94246bea3a9b4fd823
marschkc5907 <113460042+marschkc5907@users.noreply.github.com>
2022-10-02 14:35:08 +0200

    Add files via upload

---
+++ P2LAB_Stats_MarschkeCassandra.py
@@ -0,0 +1,10 @@
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
+
+your_value1 = num1 * num2 * num3 * num4 
+your_value2 = ((num1 + num2 + num3 + num4) /4)
+
+print(f'{your_value1:.0f} {your_value2:.0f}')
+print(f'{your_value1:.3f} {your_value2:.3f}')
